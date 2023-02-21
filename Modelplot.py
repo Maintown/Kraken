862f644cba670299195d2fae57f801d062bdac91
Primezane <124764288+Primezane@users.noreply.github.com>
2023-02-21 20:29:32 +0700

    add : Modelplot

--- Modelplot.py
+++ Modelplot.py
@@ -3,27 +3,34 @@ import matplotlib.pyplot as plt
 plt.style.use('seaborn-whitegrid')
 
 class Modelplot:
+    """
+    Modelplot is used for visualize deep learning train process
+    The plot is divided by 2, Trainplot and Testplot
+    
+    >>> mplot = Modelplot(history)
+    >>> mplot.Trainplot()
+    >>> mplot.Testplot()
+    """
     def __init__(self, history):
-        self.len = len(history.history['loss'])
-        self.loss = history.history['loss']
-        self.accuracy = history.history['accuracy']
-        self.val_loss = history.history['val_loss']
-        self.val_accuracy = history.history['val_accuracy']
+        self.len = range(len(history.history['loss']))
+        self.history = history
+        
+    def Testplot(self):
+        fig, ax = plt.subplots(nrows=2, ncols=1, constrained_layout=True, sharex=True)
+        ax[0].plot(self.len, self.history.history['val_loss'], label='loss', marker='x', color='red')
+        ax[1].plot(self.len, self.history.history['val_accuracy'], label='accuracy', marker='x')
+        ax[0].set_title("Test Loss")
+        ax[1].set_title("Test Accuracy")
+        ax[0].legend()
+        ax[1].legend()
+        plt.xticks(self.len);
 
-    def Plotloss(self):
-        plt.plot(self.len, self.loss, label='Train')
-        plt.plot(self.len, self.val_loss, label='Val')
-        plt.title("Lossrate Plot")
-        plt.xlabel("Epoch")
-        plt.ylabel("Loss rate")
-        plt.xticks([*range(self.len)])
-        plt.show()
-
-    def Plotacc(self):
-        plt.plot(self.len, self.accuracy, label='Train')
-        plt.plot(self.len, self.val_accuracy, label='Val')
-        plt.title("Accuracy Plot")
-        plt.xlabel("Epoch")
-        plt.ylabel("Accuracy rate")
-        plt.xticks([*range(self.len)])
-        plt.show()
+    def Trainplot(self):
+        fig, ax = plt.subplots(nrows=2, ncols=1, constrained_layout=True, sharex=True)
+        ax[0].plot(self.len, self.history.history['loss'], label='loss', marker='x', color='red')
+        ax[1].plot(self.len, self.history.history['accuracy'], label='accuracy', marker='x')
+        ax[0].set_title("Train Loss")
+        ax[1].set_title("Train Accuracy")
+        ax[0].legend()
+        ax[1].legend()
+        plt.xticks(self.len);
